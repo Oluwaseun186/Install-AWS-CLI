7500dca22796fb61fa230223653704d797f0a559
oluwaseun186 <adewumibode7@gmail.com>
2024-12-02 13:37:01 +0100

    file uploads

--- checker.sh
+++ checker.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
  
- sudo apt upgrade
+apt-get upgrade
 
- sudo apt update
+apt-get update
+
+apt-get install awscli
+
+#update the latest aws cli
+curl -o awscliv2.sig https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip.sig
+
+#unzip
+unzip awscliv2.zip
+
+#install aws cli and update
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+
+#verify the signature
+gpg --verify awscliv2.sig awscliv2.zip
+
+# Check if AWS CLI is installed
+aws --version
 
 
